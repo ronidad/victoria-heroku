f3ded586baf70e43dbdca6876c4a864f73c0eab1
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-26 19:18:37 +0300

    changed the name of the file

--- victoria.py
+++ victoria.py
@@ -7,25 +7,23 @@ import plotly.express as px
 
 crops=pd.read_csv("df_crops.csv").set_index("Produce_Variety")
 
-st.title("Average Prices chart")
-st.subheader("subhead average prices")
-st.bar_chart(crops)
 
 
-# selected_status = st.sidebar.selectbox(
-#     "Select Data to view",
-#     options=[
-#         "Average Prices Chart",
-#         "Average Prices Table",
-#     ],
-# )
 
-# if selected_status=="Average Prices Chart":
-#     st.title("Average Prices chart")
-#     st.subheader("subhead average prices")
-#     st.bar_chart(crops)
-# if selected_status=="Average Prices Table":
-#     st.title("Average Prices Table")
-#     st.subheader("subhead average prices")
-#     st.table(crops)
+selected_status = st.sidebar.selectbox(
+    "Select Data to view",
+    options=[
+        "Average Prices Chart",
+        "Average Prices Table",
+    ],
+)
+
+if selected_status=="Average Prices Chart":
+    st.title("Average Prices chart")
+    st.subheader("subhead average prices")
+    st.bar_chart(crops)
+if selected_status=="Average Prices Table":
+    st.title("Average Prices Table")
+    st.subheader("subhead average prices")
+    st.table(crops)
 
