5f553ca7eb68824fb8e81ec98e6fe1d69d6ad0bc
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-26 18:39:25 +0300

    remorte file

---
+++ streamlit.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Crops")
+crops=pd.read_csv("df_crops.csv").set_index("Produce_Variety")
+
+
+selected_status = st.sidebar.selectbox(
+    "Select Data to view",
+    options=[
+        "Average Prices Chart",
+        "Average Prices Table",
+    ],
+)
+
+if selected_status=="Average Prices Chart":
+    st.title("Average Prices chart")
+    st.subheader("subhead average prices")
+    st.bar_chart(crops)
+if selected_status=="Average Prices Table":
+    st.title("Average Prices Table")
+    st.subheader("subhead average prices")
+    st.table(crops)
+
